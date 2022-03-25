5d8de8e4cf3ccbab18f4c8077eaa51f5261a6abf
Jan Malczewski <j.t.b.malczewski@gmail.com>
2022-03-25 11:27:50 +0100

    setting files

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestCSV(unittest.TestCase):
+
+    def test_clear(self):
+        self.assertEqual(1,1,"test file isn't working")
+
+if __name__ == '__main__':
+    unittest.main()
